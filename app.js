d34d911ff71e649a504732674996721d7be21c69
Dunya4 <dunyaliyeva9@gmail.com>
2026-06-19 22:50:17 +0400

    end

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
-const button =document.querySelector(".card_btn");
-const confirm = document.querySelector(".confirm_active");
-const cardMain =document.querySelector(".card");
-const mail = document.querySelector(".card_mail");
+const button = document.querySelector(".card_btn")
+const sendMail = document.querySelector(".send_mail");
 const error = document.querySelector("#error");
+const cardMain = document.querySelector(".card");
+const active = document.querySelector(".confirm_active");
+const activeBtn = document.querySelector(".active_btn")
+const mail =document.querySelector(".card_mail")
 
-button.addEventListener("click", function(e){
-    const emailValue = mail.value;
+button.addEventListener("click" , function(e){
+    const emailValue = mail.value
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     e.preventDefault();
-    confirm.classList.add("show")
-    cardMain.classList.remove("hidden")
 
-    
     if(!emailPattern.test(emailValue)){
-        error.textContent = "Düzgün email daxil edin";
+      error.textContent = "Valid email required";
+      mail.classList.add("error_mode");     
     } else{
-    error.textContent = "";
-    cardMain.classList.add("hidden")
-    confirm.classList.remove("show")
+        sendMail.textContent = mail.value;
+        mail.value = "";
+        error.textContent = "";
+        mail.classList.remove("error_mode");
+        cardMain.classList.add("hidden")
+        active.classList.add ("show");
     }
+})
+activeBtn.addEventListener("click", function(e){
+     active.classList.remove("show");
+     cardMain.classList.remove("hidden");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const button =document.querySelector(".card_btn");
+// const confirm = document.querySelector(".confirm_active");
+// const cardMain =document.querySelector(".card");
+// const mail = document.querySelector(".card_mail");
+// const error = document.querySelector("#error");
+
+// button.addEventListener("click", function(e){
+//     const emailValue = mail.value;
+//     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     e.preventDefault();
+//     confirm.classList.add("show")
+//     cardMain.classList.remove("hidden")
+
+    
+//     if(!emailPattern.test(emailValue)){
+//         error.textContent = "Düzgün email daxil edin";
+//     } else{
+//     error.textContent = "";
+//     cardMain.classList.add("hidden")
+//     confirm.classList.remove("show")
+//     }
     
 
-})
+// })
